0901f6e68d2216773e80e34fd093ec705c88363b
iki1uc <entertrainer@web.de>
2026-07-01 19:12:58 +0200

    Update nanomes3.js

--- nanomes3.js
+++ nanomes3.js
@@ -1,1 +1,12 @@
+export const nanomes3 = {
+    nano: 0.000001,
+    max: 1.0,
+    min: 0.0
+};
+
+export function norm(v) {
+    if (v < nanomes3.min) return nanomes3.min;
+    if (v > nanomes3.max) return nanomes3.max;
+    return v;
+}
 
